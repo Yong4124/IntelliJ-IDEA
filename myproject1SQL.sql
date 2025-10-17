e9edf8776550966d76bcbc2290c3c0dc6abfc5c5
Yong4124 <dndyd4323@naver.com>
2025-10-17 17:08:47 +0900

    FEAT: 251017수업 newsecurity, restfull

--- myproject1SQL.sql
+++ myproject1SQL.sql
@@ -1,3 +1,27 @@
 create	database myproject1;
 
 use myproject1;
+
+select * from user;
+select * from comment;
+select * from post;
+DESC comment;
+
+DELETE FROM comment WHERE post_id IS NULL;
+ALTER TABLE comment MODIFY post_id bigint NOT NULL;
+
+SELECT * FROM post WHERE id = 5;
+
+DELETE FROM post;
+
+ALTER TABLE comment DROP FOREIGN KEY `FKs1slvnkuemjsq2kj4h3vhx7i1`;
+
+
+ALTER TABLE comment
+  ADD CONSTRAINT fk_comment_post
+  FOREIGN KEY (post_id) REFERENCES post(id)
+  ON DELETE CASCADE;
+
+SHOW CREATE TABLE comment;
+SHOW CREATE TABLE post_image_paths;
+
---
+++ newsec/src/main/java/com/du/newsec/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.du.newsec.config;
+
+import com.du.newsec.repository.UserRepository;
+import com.du.newsec.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/register", "/css/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout?logout")
+                        .permitAll()
+                );
+        return http.build();
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
