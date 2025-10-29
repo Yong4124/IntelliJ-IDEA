2c4c11e3a2d97c6595c0cce6c47636c565cf284a
Yong4124 <dndyd4323@naver.com>
2025-10-29 18:56:26 +0900

    FEAT: RabbitMQ예제, msa만들기

---
+++ 20251029RabbitMQ1/src/main/java/com/example/rabbitmq1/repository/ProductMessageRepository.java
@@ -0,0 +1,8 @@
+package com.example.rabbitmq1.repository;
+
+import com.example.rabbitmq1.entity.ProductMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductMessageRepository extends JpaRepository<ProductMessage, Long> {}
+
+
